aa8126c0fa025194e40be0925a01bd971156a6d3
Sergey Yevtushik <sergey@yevtushik.online>
2018-08-11 18:55:27 +0300

    dominator

--- ClassLibrary1/Codility/Leader/Dominator/Solution.cs
+++ ClassLibrary1/Codility/Leader/Dominator/Solution.cs
@@ -8,9 +8,52 @@ namespace AlgorithmicPractice.Codility.Leader.Dominator
     {
         public int solution(int[] A)
         {
+            if (A.Length == 0)
+                return -1;
 
+            int size = 1;
+            int val = A[0];
+            for(int i = 1; i < A.Length; i++)
+            {
+                if(size == 0)
+                {
+                    val = A[i];
+                    size = 1;
+                }
+                else
+                {
+                    if(A[i] == val)
+                    {
+                        size++;
+                    }
+                    else
+                    {
+                        size--;
+                    }
+                }
+            }
 
-            return 0;
+            int candidate;
+            if (size > 0)
+                candidate = val;
+            else
+                return -1;
+
+            int cnt = 0;
+            int pointer = -1;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == candidate)
+                {
+                    cnt++;
+                    pointer = i;
+                }
+            }
+
+            if (cnt > A.Length / 2)
+                return pointer;
+            else
+                return -1;
         }
     }
 }
--- Tests/Codility/Leader/Dominator.cs
+++ Tests/Codility/Leader/Dominator.cs
@@ -14,7 +14,37 @@ namespace Tests.Codility.Leader
 
             var res = sln.solution(new int[] { 3, 4, 3, 2, 3, -1, 3, 3 });
 
+            Assert.AreEqual<int>(7, res);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution sln = new Solution();
+
+            var res = sln.solution(new int[] { 3, 4, 3, 2, 3, -1, 3 });
+
+            Assert.AreEqual<int>(6, res);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution sln = new Solution();
+
+            var res = sln.solution(new int[] { 3, 3, 3, 3, 1, 1, 1 });
+
             Assert.AreEqual<int>(3, res);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution sln = new Solution();
+
+            var res = sln.solution(new int[] { 3,  1 });
+
+            Assert.AreEqual<int>(-1, res);
         }
     }
 }
