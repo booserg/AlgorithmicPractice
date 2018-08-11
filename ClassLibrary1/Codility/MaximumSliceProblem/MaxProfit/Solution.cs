5158ef4d5a65a6014dc6ba6f7f61011b8de4a7f0
Sergey Yevtushik <sergey@yevtushik.online>
2018-08-11 19:49:39 +0300

    MaxProfit

---
+++ ClassLibrary1/Codility/MaximumSliceProblem/MaxProfit/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmicPractice.Codility.MaximumSliceProblem.MaxProfit
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            if (A.Length == 0)
+                return 0;
+
+            int absMin = A[0];
+            int maxAfterMinUpdated = A[0];
+            int maxProfit = 0;
+            for(int i = 1; i < A.Length;i++)
+            {
+                if(A[i] < absMin)
+                {
+                    absMin = A[i];
+                    maxAfterMinUpdated = A[i];
+                }
+
+                if (A[i] > maxAfterMinUpdated)
+                    maxAfterMinUpdated = A[i];
+
+                if (maxProfit < maxAfterMinUpdated - absMin)
+                    maxProfit = maxAfterMinUpdated - absMin;
+            }
+
+            return maxProfit;
+        }
+    }
+}
+
---
+++ Tests/Codility/MaximumSliceProblem/MaxProfit.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmicPractice.Codility.MaximumSliceProblem.MaxProfit;
+
+namespace Tests.Codility.MaximumSliceProblem
+{
+    [TestClass]
+    public class MaxProfit
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution sln = new Solution();
+
+            var res = sln.solution(new int[] {23171, 21011, 21123, 21366, 21013, 21367 });
+
+            Assert.AreEqual<int>(356, res);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution sln = new Solution();
+
+            var res = sln.solution(new int[] { 1, 2, 1, 3, 1 });
+
+            Assert.AreEqual<int>(2, res);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution sln = new Solution();
+
+            var res = sln.solution(new int[] { 1, 2, 1, 3, 3, 1 });
+
+            Assert.AreEqual<int>(2, res);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution sln = new Solution();
+
+            var res = sln.solution(new int[] { 0, 3, 1, 2, 0 });
+
+            Assert.AreEqual<int>(3, res);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution sln = new Solution();
+
+            var res = sln.solution(new int[] { 0, 1, 2, 3, 2 });
+
+            Assert.AreEqual<int>(3, res);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Solution sln = new Solution();
+
+            var res = sln.solution(new int[] { 2, 3, 2, 1, 0 });
+
+            Assert.AreEqual<int>(1, res);
+        }
+    }
+}
